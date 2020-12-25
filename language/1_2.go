b392a5518bfd03fd29a3a70e5582983caa639b10
Survivor75 <ashutoshnarang80@gmail.com>
2020-12-26 00:06:52 +0530

    fix confusion

--- language/1_2.go
+++ language/1_2.go
@@ -7,7 +7,6 @@ const grams = 100.0
 
 type amu float64 
 
-// function float accepts value of type mass and returns a type of float64 [?]
 func (mass amu) float() float64 { 
   return float64(mass) 
 } 
@@ -18,7 +17,6 @@ type metalloid struct {
   weight amu 
 }
 
-// metalloids is a slice of type metalloid
 var metalloids = []metalloid{ 
   metalloid{"Boron", 5, 10.81}, 
   metalloid{"Silicon", 14, 28.085}, 
@@ -29,12 +27,10 @@ var metalloids = []metalloid{
   metalloid{"Polonium", 84, 209.0}, 
 } 
 
-// function moles accepts value of type mass and returns a type of float64 [?]
 func moles(mass amu) float64 { 
   return float64(mass) / grams 
 } 
 
-// function atoms accepts value of type float64 and returns a type of float64 [?]
 func atoms(moles float64) float64 { 
   return moles * avogadro 
 } 
